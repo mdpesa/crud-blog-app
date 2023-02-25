0a664f385a691820c4dde1d02026e75e5dd4a9b9
Marta Santos <mdpesa@gmail.com>
2023-02-25 20:59:41 +0100

    add Modal

--- src/components/pages/Post/Post.js
+++ src/components/pages/Post/Post.js
@@ -1,12 +1,24 @@
-import { getPostById } from '../../../redux/postsRedux';
-import { useSelector } from 'react-redux';
+import { getPostById, removePost } from '../../../redux/postsRedux';
+import { useDispatch, useSelector } from 'react-redux';
 import { NavLink, useParams } from 'react-router-dom';
 import { Navigate } from 'react-router-dom';
-import { Button, Card, Col, Row } from 'react-bootstrap';
+import { Button, Card, Col, Row, Modal } from 'react-bootstrap';
+import { useState } from 'react';
 
 const Post = () => {
   const { postId } = useParams();
   const postData = useSelector((state) => getPostById(state, postId));
+
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  const dispatch = useDispatch();
+  const postOut = (id) => {
+    dispatch(removePost(id));
+  };
+
   if (!postData) return <Navigate to='/' />;
   return (
     <>
@@ -26,12 +38,45 @@ const Post = () => {
           </Card>
         </Col>
         <Col className='d-flex justify-content-end align-items-center'>
-          <Button variant='outline-info' as={NavLink} to={'/post/edit/' + postId}>
+          <Button
+            variant='outline-info'
+            as={NavLink}
+            to={'/post/edit/' + postId}
+          >
             Edit
           </Button>
-          <Button variant='outline-danger'>Delete</Button>
+          <Button
+            variant='outline-danger'
+            className='ms-2 sm-9'
+            onClick={handleShow}
+          >
+            Delete
+          </Button>
         </Col>
       </Row>
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Are you sure?</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          This operation will completely remove this post from the app. <br />{' '}
+          Are you sure you want to do that?
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant='secondary' onClick={handleClose}>
+            Cancel
+          </Button>
+          <Button
+            variant='danger'
+            onClick={() => {
+              handleClose();
+              postOut(postData.id);
+            }}
+          >
+            Remove
+          </Button>
+        </Modal.Footer>
+      </Modal>
     </>
   );
 };
--- src/redux/postsRedux.js
+++ src/redux/postsRedux.js
@@ -4,10 +4,15 @@ export const getPostById = ({ posts }, postId) =>
   posts.find((post) => post.id === postId);
 // actions
 const createActionName = (actionName) => `app/posts/${actionName}`;
+const REMOVE_POST = createActionName('REMOVE_POST');
 
 // action creators
+export const removePost = (payload) => ({ type: REMOVE_POST, payload });
+
 const postsReducer = (statePart = [], action) => {
   switch (action.type) {
+    case REMOVE_POST:
+      return [...statePart.filter((posts) => posts.id !== action.payload)];
     default:
       return statePart;
   }
